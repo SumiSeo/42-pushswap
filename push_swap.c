c928359e3e1f98659e318474174cc25fffc62101
Sumi Seo <sumseo@bess-f3r2s6.clusters.42paris.fr>
2024-04-11 16:27:03 +0200

    feat/delete-redundant

--- push_swap.c
+++ push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:06:16 by sumseo            #+#    #+#             */
-/*   Updated: 2023/12/23 17:59:06 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/04/11 16:26:23 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,21 @@
 
 int	push_swap(char *num_list)
 {
-	printf("num list: %s\n", num_list);
-	return (2);
+	// printf("num list: %s\n", num_list);
+	// return (2);
 }
 int	main(int argc, char *argv[])
 {
-	t_stack_node	*a;
-
-
-	a = NULL;
-
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-		return (1);
-	else if (argc == 2)
-	{
-		argv = ft_split(argv[1], ' ');
-		create_stack(&a, argv);
-	}
-	else
-		create_stack(&a, argv + 1);
-
-	return (1);
+	// t_stack_node	*a;
+	// a = NULL;
+	// if (argc == 1 || (argc == 2 && !argv[1][0]))
+	// 	return (1);
+	// else if (argc == 2)
+	// {
+	// 	argv = ft_split(argv[1], ' ');
+	// 	create_stack(&a, argv);
+	// }
+	// else
+	// 	create_stack(&a, argv + 1);
+	// return (1);
 }
--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sumseo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:04:46 by sumseo            #+#    #+#             */
-/*   Updated: 2023/12/12 15:06:11 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/04/11 16:26:39 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,4 @@
 # include <string.h>
 # include <unistd.h>
 
-typedef struct s_stack_node
-{
-	int					value;
-	int					current_position;
-	int					final_index;
-	int					push_price;
-	bool				above_median;
-	bool				above_cheapest;
-	struct s_stack_node	*target_node;
-	struct s_stack_node	*next;
-	struct s_stack_node	*prev;
-}						t_stack_node;
-
-void					create_stack(t_stack_node **a, char **argv);
-t_stack_node			*find_last_node(t_stack_node *head);
-void					append_stack(t_stack_node **stack, int n);
-void					sa(t_stack_node **a);
-void					sb(t_stack_node **b);
-void					pa(t_stack_node **a, t_stack_node **b);
-void					pb(t_stack_node **a, t_stack_node **b);
-t_stack_node			*find_first_node(t_stack_node *head);
-void					prepend_stack(t_stack_node **stack, int n);
 #endif
